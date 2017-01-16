c6571ec887eb4920b6b4d2f9ed978ab44c903d80
Jesus Gutierrez <jgutierrez@devsu.com>
2017-01-16 15:42:27 -0500

    Error handlers test, refactor proxy.specs.js and proxy.js

--- spec/util/mocks.js
+++ spec/util/mocks.js
@@ -10,6 +10,42 @@ class GreeterServiceMock {
   sayGoodbye() {} // eslint-disable-line
 }
 
+class MiddlewareMock {
+  constructor() {
+    this.globalMiddleware = {'scope': '*', 'method': Spy.create()};
+    this.packageMiddleware = {'scope': 'testapp', 'method': Spy.create()};
+    this.packageMiddleware2 = {'scope': 'greeter', 'method': Spy.create()};
+    this.packageMiddleware3 = {'scope': 'PersonService', 'method': Spy.create()};
+    this.serviceMiddleware = {'scope': 'testapp.PersonService', 'method': Spy.create()};
+    this.serviceMiddleware2 = {'scope': 'testapp.GreeterService', 'method': Spy.create()};
+    this.methodMiddleware = {'scope': 'testapp.PersonService.list', 'method': Spy.create()};
+    this.methodMiddleware2 = {'scope': 'testapp.PersonService.get', 'method': Spy.create()};
+    this.middleware = [
+      this.globalMiddleware, this.packageMiddleware, this.packageMiddleware2,
+      this.packageMiddleware3, this.serviceMiddleware, this.serviceMiddleware2,
+      this.methodMiddleware, this.methodMiddleware2,
+    ];
+  }
+}
+
+class ErrorHandlerMock {
+  constructor() {
+    this.globalErrorHandler = {'scope': '*', 'method': Spy.create()};
+    this.packageErrorHandler = {'scope': 'testapp', 'method': Spy.create()};
+    this.packageErrorHandler2 = {'scope': 'greeter', 'method': Spy.create()};
+    this.packageErrorHandler3 = {'scope': 'PersonService', 'method': Spy.create()};
+    this.serviceErrorHandler = {'scope': 'testapp.PersonService', 'method': Spy.create()};
+    this.serviceErrorHandler2 = {'scope': 'testapp.GreeterService', 'method': Spy.create()};
+    this.methodErrorHandler = {'scope': 'testapp.PersonService.list', 'method': Spy.create()};
+    this.methodErrorHandler2 = {'scope': 'testapp.PersonService.get', 'method': Spy.create()};
+    this.errorHandlers = [
+      this.globalErrorHandler, this.packageErrorHandler, this.packageErrorHandler2,
+      this.packageErrorHandler3, this.serviceErrorHandler, this.serviceErrorHandler2,
+      this.methodErrorHandler, this.methodErrorHandler2,
+    ];
+  }
+}
+
 module.exports = class Mocks {
   static getServer() {
     return {
@@ -59,5 +95,12 @@ module.exports = class Mocks {
     return greeterServiceMock;
   }
 
+  static getMiddleware() {
+    return new MiddlewareMock();
+  }
+
+  static getErrorHandler() {
+    return new ErrorHandlerMock();
+  }
 };
 
