0ca940ff4583afd889bf8ec1d65caba6b2e9ebff
Jesus Gutierrez <jgutierrez@devsu.com>
2017-01-19 10:18:08 -0500

    Integration test for simple and unidirectional stream

--- spec/integration/condor-integration.spec.js
+++ spec/integration/condor-integration.spec.js
@@ -1,14 +1,13 @@
 const Condor = require('../../lib/condor');
 const grpc = require('grpc');
 
+
 class Person {
   list() {} // eslint-disable-line
-
   get() {} // eslint-disable-line
 }
 class Greeter {
-  sayGoodbye() {} // eslint-disable-line
-
+  sayGoodbye(call) {} // eslint-disable-line
   sayHello(call) {
     const response = {'greeting': call.request.greeting};
     return Promise.resolve(response);
@@ -16,8 +15,23 @@ class Greeter {
 }
 
 class Car {
-  list() {
-    return [{'id': 1, 'name': 'ford fiesta'}];
+  insert(call) {
+    const car = call.request;
+    if (this._carStream) {
+      this._carStream.write(car);
+    }
+    return Promise.resolve(call.request);
+  }
+
+  carLog(stream) {
+    this._carStream = stream;
+  }
+
+  log(stream) {
+    stream.on('data', function(data) {});
+    stream.on('end', function() {
+      stream.sendMetadata(stream.metadata);
+    });
   }
 }
 
@@ -37,14 +51,59 @@ describe('condor framework', () => {
 
   it('should receive client request and respond with the same request object', (done) => {
     const greeterProto = grpc.load('spec/protos/greeter.proto');
-    const client = new greeterProto.testapp.greeter.GreeterService('127.0.0.1:9999',
+    const greeterClient = new greeterProto.testapp.greeter.GreeterService('127.0.0.1:9999',
       grpc.credentials.createInsecure());
-    const greeter = {'greeting': 'hola greeting'};
-    client.sayHello(greeter, (error, greeting) => {
-      expect(greeting).toEqual(greeter);
+    const expectedGreeter = {'greeting': 'hello greeting'};
+    greeterClient.sayHello(expectedGreeter, (error, greeterResponse) => {
+      expect(greeterResponse).toEqual(expectedGreeter);
       expect(error).toBeNull();
       done();
     });
+  });
+
+  it('should receive client object and respond with a stream', (done) => {
+    const carProto = grpc.load('spec/protos/car.proto');
+    const carClient = new carProto.transport.land.CarService('127.0.0.1:9999',
+      grpc.credentials.createInsecure());
+    const expectedCar = {'id': 1, 'name': 'mustang'};
+    const stream = carClient.carLog(expectedCar);
+    stream.on('data', (carResponse) => {
+      expect(carResponse).toEqual(expectedCar);
+      done();
+    });
+    carClient.insert(expectedCar, (error, carResponse) => {
+      expect(carResponse).toEqual(expectedCar);
+      expect(error).toBeNull();
+    });
+  });
+
+  it('should receive a stream and respond with an object', (done) => {
+    const expectedCar = {'id': 1, 'name': 'mustang'};
+    const car = new Car();
+    car.log = (stream) => {
+      stream.on('data', function(data) {
+        console.log('steaming');
+        expect(data).toEqual(expectedCar);
+      });
+      stream.on('end', function(data) {
+        // expect(data).not.toBeUndefined();
+        console.log('data', data);
+        done();
+      });
+    };
+    condor = new Condor()
+      .addService('spec/protos/car.proto', 'transport.land.CarService', car)
+      .start();
+
+    const carProto = grpc.load('spec/protos/car.proto');
+    const carClient = new carProto.transport.land.CarService('127.0.0.1:9999',
+      grpc.credentials.createInsecure());
+    const stream = carClient.log({}, (error, value) => {
+      expect(error).toBeNull();
+      console.log('value', value, 'value');
+    });
+    stream.write(expectedCar);
+    stream.end();
   });
 
   afterAll(() => {
