ca5f9831a8b494946477b21633ff02ec08fd40ad
Cesar Salazar <csalazar@devsu.com>
2017-01-20 17:01:05 -0500

    integration tests

---
+++ spec/integration/repeater.js
@@ -0,0 +1,39 @@
+const Promise = require('bluebird');
+
+module.exports = class {
+  simple(call) {
+    return this._buildResponse(call.request.message, 1);
+  }
+
+  streamToServer(stream) {
+    let messages = '';
+    return new Promise((resolve) => {
+      stream.on('data', (data) => {
+        messages = messages.concat(data.message);
+      });
+      stream.on('end', () => {
+        resolve(this._buildResponse(messages));
+      });
+    });
+  }
+
+  streamToClient(stream) {
+    const message = stream.request.message;
+    stream.write(this._buildResponse(message));
+    stream.write(this._buildResponse(message));
+    stream.end();
+  }
+
+  bidirectionalStream(stream) {
+    stream.on('data', (data) => {
+      stream.write(this._buildResponse(data.message));
+    });
+    stream.on('end', () => {
+      stream.end();
+    });
+  }
+
+  _buildResponse(message) {
+    return {'message': `You sent: '${message}'.`};
+  }
+};
